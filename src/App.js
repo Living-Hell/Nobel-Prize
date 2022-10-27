6d129ea4503829f9a3aed8c73d445a69f8102e76
Shushant Kumar <s.living.hell@gmail.com>
2022-10-28 00:07:14 +0530

    Adding the list

--- src/App.js
+++ src/App.js
@@ -5,29 +5,9 @@ import { useEffect } from 'react';
 
 function App() {
 
-  const [winnerList, setWinnerList] = useState([]);
-  useEffect(() => {
-    async function fetchWinnerList(){
-      try {
-        const requestUrl = 'http://api.nobelprize.org/v1/prize.json';
-        const response = await fetch(requestUrl);
-        const responseJSON = await response.json();
-        console.log(responseJSON.prizes[0].year);
-        setWinnerList(responseJSON);
-      } catch {
-        
-      }
-    }
-  
-    return () => {
-      fetchWinnerList();
-    }
-  }, [])
-  
-
   return (
     <div className='App'>
-      <NobelPrize winnersList={winnerList} />
+      <NobelPrize />
     </div>
   );
 }
--- src/components/NobelPrize.js
+++ src/components/NobelPrize.js
@@ -1,22 +1,107 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import { Container, createTheme, LinearProgress, makeStyles, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TextField, ThemeProvider, Typography } from '@material-ui/core';
+import { Pagination } from '@material-ui/lab';
 
-function NobelPrize(props) {
-    const {winnerList} = props;
-  return (
-    <ul>
-        {winnerList.map(post=>(
-            <li>{post.prizes[0].year}</li>
-        ))}
-    </ul>
-  )
+const useStyles = makeStyles(() => ({
+    row: {
+        backgroundColor: "#101a27",
+        cursor: "pointer",
+        "&:hover": {
+            backgroundColor: "#091217",
+        },
+        fontFamily: "Roboto",
+    },
+    pagination: {
+        "& .MuiPaginationItem-root": {
+            color: "#87cefb",
+        },
+    },
+}));
+
+export default class NobelPrize extends React.Component {
+  state = {
+    loading: true,
+    person: null
+  };
+
+  
+
+  async componentDidMount() {
+    const url = "https://api.nobelprize.org/v1/prize.json";
+    const response = await fetch(url);
+    const data = await response.json();
+    this.setState({ person: data.prizes, loading: false });
+    console.log(data.prizes);
+  }
+
+  render() {
+    if (this.state.loading) {
+        <LinearProgress style={{ backgroundColor: "#40E0D0"}} />
+      return <div style={{ backgroundColor: "#40E0D0"}} >loading...</div>;
+    }
+
+    if (!this.state.person) {
+      return <div>didn't get a person</div>;
+    }
+
+    return (
+            <Container style = {{ textAlign: "center"}}>
+                <Typography
+                    variant = "h4"
+                    style = {{ margin:18, fontFamily: "Roboto" }}
+                    >
+                    Nobel Prize Winners
+                    </Typography>
+
+                {/* Insert Filter Here*/}
+        
+        <TableContainer>
+        <Table>
+            <TableHead style = {{backgroundColor: "#91bad6"}}>
+                <TableRow>
+                    {["Year", "Category", "Laureates"].map((head) => (
+                        <TableCell
+                        style = {{
+                            color: "black",
+                            fontWeight: "700",
+                            fontFamily: "Roboto",
+                        }}
+                        key = {head}
+                        
+                        >
+                            {head}
+                        </TableCell>
+                    ))}
+                </TableRow>
+            </TableHead>
+            <TableBody>
+                {this.state.person.map((item =>
+                    <TableRow>
+                        <TableCell >{item.year}</TableCell>
+                        <TableCell>{item.category}</TableCell>
+                        <TableCell>{item.laureates && item.laureates.map((name => 
+                            <td>{name.firstname} {" "}{name.surname}{" | "}</td>
+                            ))}
+                        </TableCell>
+                        
+                    </TableRow>
+                ))}
+            </TableBody>
+            </Table>
+                    
+            </TableContainer>
+            <Pagination
+                style = {{ 
+                    padding: 20,
+                    width: "100%",
+                    display: "flex",
+                    justifyContent: "center",
+                 }}
+                >
+            </Pagination>
+
+        </Container>
+      
+    );
+  }
 }
-
-NobelPrize.propTypes = {
-    winnerList: PropTypes.array,
-};
-NobelPrize.defaultProps = {
-    winnerList: [], 
-}
-
-export default NobelPrize
